4ee7145545bfb66fed9be9777cb86d1de89b01ea
omkar585 <omkarperekar585@gmail.com>
2026-06-13 20:09:32 +0530

    update

---
+++ Day-4/index.js
@@ -0,0 +1,39 @@
+var students = [
+    "omkar",
+    "krishna",
+    "nilesh",
+    "arpit",
+    "messi",
+    "ronaldo",
+    "neymar",
+    "omkar",
+    "krishna",
+    "arpit",
+    "messi",
+    "ronaldo",
+    "neymar",
+    "omkar",
+    "krishna",
+    "nilesh",
+    "arpit",
+    "messi",
+    "ronaldo",
+    "neymar",
+
+];
+
+console.log(students);
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[3]);
+
+console.log(students.length);
+console.log(students.length - 1);
+
+console.log([students.length - 1] / 2);
+console.log(students[(students.length - 1) / 2]);
+
+
+students[26] = " heyy ";
+console.log(students);
+console.log(students[26]);
